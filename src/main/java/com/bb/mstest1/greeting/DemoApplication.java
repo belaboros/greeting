adc4e43066f00b752dcf9e9ddeabaf010c54448a
bb_jx_test <bela.boros@gmail.com>
2018-12-09 01:54:21 +0100

    Initial import

---
+++ src/main/java/com/bb/mstest1/greeting/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.bb.mstest1.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
